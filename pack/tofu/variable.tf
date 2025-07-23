fbaf7bb70938a0afeacf0c07adbf88f429912539
Chris Marget <cmarget@juniper.net>
2025-07-23 17:59:31 -0400

    update variable.tf

--- pack/tofu/variable.tf
+++ pack/tofu/variable.tf
@@ -11,35 +11,14 @@ variable "name" {
   default = "STIG ntp"
 }
 
-# variable "servers" {
-#   type = list(string)
-#   description = "List of NTP server IP addresses, must have the same length as the `keys` list."
-#   default = [
-#     "192.2.0.11",
-#     "192.2.0.12",
-#   ]
-# }
-#
-# variable "keys" {
-#   type = list(string)
-#   description = "List of SHA256 key strings, must have the same length as the `servers` list."
-#   default = [
-#     "foo",
-#     "bar",
-#   ]
-# }
-
 variable "ntp_servers" {
   type = map (string)
-  description = "Map of NTP authentication strings keyed by server IP address."
+  description = "Map of NTP SHA256 authentication strings keyed by server IP address."
   default = {
-    "192.2.0.11" = "foo"
-    "192.2.0.12" = "bar"
   }
 }
 
 variable "source_ip" {
   type = string
   description = "Source IP to be used when sending NTP traffic."
-  default = "192.2.0.1"
 }
